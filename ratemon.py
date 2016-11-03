d3b39c18eb79e800e208f174b2477e5dad352a53
Lars Nielsen <lnc13.lars@gmail.com>
2016-11-03 14:28:46 +0100

    update to license text

--- ratemon.py
+++ ratemon.py
@@ -1,9 +1,10 @@
-'''
+#!/usr/bin/env python
+"""
  Copyright (c) Steinwurf ApS 2016.
  All Rights Reserved
 
  Distributed under the "BSD License". See the accompanying LICENSE.rst file.
-'''
+"""
 class ratemon():
     """Monitor object."""
 
