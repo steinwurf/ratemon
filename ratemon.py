d0dea6f2c148ef9df991554c424cac93b1925eb8
Lars Nielsen <lnc13.lars@gmail.com>
2016-11-07 12:44:01 +0100

    continued test of UI

--- ratemon.py
+++ ratemon.py
@@ -310,7 +310,7 @@ class ratemon():
             received = station['received']
             fps = station['fps']
             ## Calculated average in Kb
-            station['average'] = (received / fps) / 1000
+            station['average'] = (received / fps) #/ 1000
 
             ## Reset data counters
             station['start'] = now
